85a9ae58dcd526a823c0abf1e134fb27a0d4432d
Andrius <andrius2500@gmail.com>
2023-01-23 15:40:25 +0200

    small fix

--- src/api/api.types.ts
+++ src/api/api.types.ts
@@ -4,7 +4,7 @@ export interface MovieListItemResponse {
   genre_ids: number[];
   id: number;
   original_language: string;
-  original_title?: string;
+  original_title: string;
   overview: string;
   popularity: number;
   poster_path: string | null;
